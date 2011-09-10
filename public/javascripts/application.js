931bc9f63e8c9a07d2e2d1bf63462b308d65e027
art <art@darkruby.co.uk>
2011-09-10 08:23:27 +0100

    social plugins on product page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -23,3 +23,10 @@ $(function() {
     });
   });
 });
+
+//plus1
+(function() {
+  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+  po.src = 'https://apis.google.com/js/plusone.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
