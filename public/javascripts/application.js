292d8dc500795ba7a7c2050cb5f1341f5756c850
art <art@darkruby.co.uk>
2012-01-05 21:44:38 +0000

    css enhancements;bye gooey menu;alternate rows;

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,9 +3,6 @@ $(function() {
   $('#templatemo_menu a,.sidebar_title').corner('5px');
   $('.darkruby').corner('3px');
 
-  //lava lamp style menu
-  //gooeymenu.setup({id:'menu'});
-
   //slides
   $("#templatemo_banner").slides({
     play: 4000,
